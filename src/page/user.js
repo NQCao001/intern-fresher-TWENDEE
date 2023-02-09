3e1826b6dd9459a081ed94ce3b777cdb9f775126
NQCao001 <nguyencao3997@gmail.com>
2023-02-09 08:49:23 +0700

    RM

--- src/page/user.js
+++ src/page/user.js
@@ -1,6 +1,6 @@
 import {useDispatch, useSelector} from "react-redux";
 import React, {useEffect, useState} from 'react';
-import { Pagination } from 'antd';
+import {Pagination} from 'antd';
 import {showInfo} from "../service/userService";
 
 const User = () => {
@@ -19,7 +19,9 @@ const User = () => {
     return (
         <>
             <div>
-                <h3>Profile</h3>
+                <center>
+                    <h3>Profile</h3>
+                </center>
                 <table className="table">
                     <thead>
                     <tr>
@@ -29,10 +31,10 @@ const User = () => {
                         <th>Thumbnail Icon</th>
                     </tr>
                     </thead>
-                    {dataUser && dataUser.map((item,index) => (
+                    {dataUser && dataUser.map((item, index) => (
                         <tbody key={index}>
                         <tr key={index}>
-                            <th>{index+1}</th>
+                            <th>{index + 1}</th>
                             <td>{item.name.title} {item.name.first} {item.name.last}</td>
                             <td>{item.login.username}</td>
                             <td><img src={item.picture.thumbnail}/></td>
@@ -45,7 +47,7 @@ const User = () => {
                 <Pagination defaultCurrent={6}
                             total={100}
                             defaultPageSize={10}
-                            onChange={(page)=>chosePage(page)}
+                            onChange={(page) => chosePage(page)}
                 />
             </center>
         </>
